cf8e2d6b211ad303c9bc0e9f00362f42fe50b12d
Pavel Hradil <umpire.github@gmail.com>
2022-12-19 11:43:44 +0100

    something wrong with namespaces - fixed - runable

--- examples/SharpBrick.PoweredUp.Examples/ExamplePavel.cs
+++ examples/SharpBrick.PoweredUp.Examples/ExamplePavel.cs
@@ -5,89 +5,90 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using SharpBrick.PoweredUp;
 
-namespace Example;
-
-public class ExamplePavel : BaseExample
+namespace Example
 {
-    public override async Task ExecuteAsync()
+    public class ExamplePavel : BaseExample
     {
-        using var hub = Host.FindByType<TwoPortHub>();
-
-        await hub.RgbLight.SetRgbColorsAsync(0x00, 0x00, 0xff);
-        //await technicMediumHub.cc
-        var volt = hub.Voltage;
-        
-        var battery = hub.BatteryVoltageInPercent;
-        Console.WriteLine($"Jedeme na {battery}%");
-
-        var motor = hub.A.GetDevice<SystemTrainMotor>();
-
-        //for (int i = 0; i < 256; i+=32)
-        //{
-        //    for(int j = 0; j < 256; j+=32)
-        //    {
-        //        for (int k = 0; k < 256; k+=32)
-        //        {
-        //            Console.Write($"\r{i}; {j}; {k}     ");
-        //            await hub.RgbLight.SetRgbColorsAsync(
-        //                (byte)i,
-        //                (byte)j,
-        //                (byte)k
-        //            );
-        //            await Task.Delay(1);
-
-        //        }
-        //    }
-        //}
-
-        sbyte rychlost = 50;
-        await motor.StartPowerAsync(10);
-        await Task.Delay(1000);
-
-        await motor.StartPowerAsync(20);
-        await Task.Delay(1000);
-
-        await motor.StartPowerAsync(30);
-        await Task.Delay(1000);        
-        
-
-        Console.WriteLine("Zmacni \"ENTER\" pro konec");
-
-        var run = true;
-
-        while (run)
+        public override async Task ExecuteAsync()
         {
-            await motor.StartPowerAsync(rychlost);
-            var key = Console.ReadKey();
-            if(key.Key == ConsoleKey.Enter)
+            using var hub = Host.FindByType<TwoPortHub>();
+
+            await hub.RgbLight.SetRgbColorsAsync(0x00, 0x00, 0xff);
+            //await technicMediumHub.cc
+            var volt = hub.Voltage;
+
+            var battery = hub.BatteryVoltageInPercent;
+            Console.WriteLine($"Jedeme na {battery}%");
+
+            var motor = hub.A.GetDevice<SystemTrainMotor>();
+
+            //for (int i = 0; i < 256; i+=32)
+            //{
+            //    for(int j = 0; j < 256; j+=32)
+            //    {
+            //        for (int k = 0; k < 256; k+=32)
+            //        {
+            //            Console.Write($"\r{i}; {j}; {k}     ");
+            //            await hub.RgbLight.SetRgbColorsAsync(
+            //                (byte)i,
+            //                (byte)j,
+            //                (byte)k
+            //            );
+            //            await Task.Delay(1);
+
+            //        }
+            //    }
+            //}
+
+            sbyte rychlost = 50;
+            await motor.StartPowerAsync(10);
+            await Task.Delay(1000);
+
+            await motor.StartPowerAsync(20);
+            await Task.Delay(1000);
+
+            await motor.StartPowerAsync(30);
+            await Task.Delay(1000);
+
+
+            Console.WriteLine("Zmacni \"ENTER\" pro konec");
+
+            var run = true;
+
+            while (run)
             {
-                run = false;
-            }
-            else if (key.Key == ConsoleKey.LeftArrow && rychlost > 0)
-            {
-                rychlost -= 10;                
-            }
-            else if (key.Key == ConsoleKey.RightArrow && rychlost < 90)
-            {
-                rychlost += 10;
+                await motor.StartPowerAsync(rychlost);
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    run = false;
+                }
+                else if (key.Key == ConsoleKey.LeftArrow && rychlost > 0)
+                {
+                    rychlost -= 10;
+                }
+                else if (key.Key == ConsoleKey.RightArrow && rychlost < 90)
+                {
+                    rychlost += 10;
+                }
+
+                await Task.Delay(10);
             }
 
-            await Task.Delay(10);
+
+            Console.ReadKey();
+
+            ////await motor.StopByBrakeAsync();
+            //await motor.StopByFloatAsync();
+            //await Task.Delay(5000);
+
+            //await motor.StartPowerAsync(-25);
+            //await Task.Delay(2000);
+
+
+
+
+            await hub.SwitchOffAsync();
         }
-
-
-        Console.ReadKey();
-
-        ////await motor.StopByBrakeAsync();
-        //await motor.StopByFloatAsync();
-        //await Task.Delay(5000);
-
-        //await motor.StartPowerAsync(-25);
-        //await Task.Delay(2000);
-
-
-
-
-        await hub.SwitchOffAsync();
     }
 }
--- examples/SharpBrick.PoweredUp.Examples/Program.cs
+++ examples/SharpBrick.PoweredUp.Examples/Program.cs
@@ -6,72 +6,73 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace SharpBrick.PoweredUp.Examples;
-
-class Program
+namespace SharpBrick.PoweredUp.Examples
 {
-    // invoke (due to multi-targeting) with ...
-    // dotnet run -f net5.0 --
-    // dotnet run -f net5.0-windows10.0.19041.0 --
 
-    // use command line parameters
-    // --EnableTrace true
-    // --BluetoothAdapter WinRT|BlueGigaBLE
-    // --COMPortName COM4
-    // --TraceDebug true
-    // or configure in poweredup.json
-    static async Task Main(string[] args)
+    class Program
     {
-        // (1) load a configuration
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Environment.CurrentDirectory)
-            .AddJsonFile("poweredup.json", true)
-            .AddCommandLine(args)
-            .Build();
+        // invoke (due to multi-targeting) with ...
+        // dotnet run -f net5.0 --
+        // dotnet run -f net5.0-windows10.0.19041.0 --
 
-        // Example Selection: select the example to execute
-        var exampleToExecute = configuration["Example"] ?? "Pavel";
-
-        var typeToExecute = Assembly
-            .GetExecutingAssembly()
-            .GetTypes()
-            .FirstOrDefault(x => x.FullName.StartsWith($"Example.Example{exampleToExecute}"));
-
-        if (typeToExecute is null)
+        // use command line parameters
+        // --EnableTrace true
+        // --BluetoothAdapter WinRT|BlueGigaBLE
+        // --COMPortName COM4
+        // --TraceDebug true
+        // or configure in poweredup.json
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Could not find example.");
+            // (1) load a configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("poweredup.json", true)
+                .AddCommandLine(args)
+                .Build();
 
-            return;
-        }
+            // Example Selection: select the example to execute
+            var exampleToExecute = configuration["Example"] ?? "Pavel";
 
-        var example = Activator.CreateInstance(typeToExecute) as Example.BaseExample;
+            var typeToExecute = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.FullName.StartsWith($"Example.Example{exampleToExecute}"));
 
-
-        // (2) build the DI container
-        var serviceCollection = new ServiceCollection();
-
-        // (2a) configure your favourite level of logging.
-        serviceCollection.AddLogging(builder =>
-        {
-            builder
-                .AddConsole();
-
-            if (bool.TryParse(configuration["EnableTrace"], out var enableTrace) && enableTrace)
+            if (typeToExecute is null)
             {
-                builder.AddFilter("SharpBrick.PoweredUp.Bluetooth.BluetoothKernel", LogLevel.Debug);
+                Console.WriteLine("Could not find example.");
+
+                return;
             }
-            if (bool.TryParse(configuration["TraceDebug"], out var traceDebug) && traceDebug)
+
+            var example = Activator.CreateInstance(typeToExecute) as Example.BaseExample;
+
+
+            // (2) build the DI container
+            var serviceCollection = new ServiceCollection();
+
+            // (2a) configure your favourite level of logging.
+            serviceCollection.AddLogging(builder =>
             {
-                builder.AddFilter("SharpBrick.PoweredUp.BlueGigaBLE.BlueGigaBLEPoweredUpBluetoothAdapater", LogLevel.Debug);
-            }
-        });
+                builder
+                    .AddConsole();
 
-        // (2b) add .AddPoweredUp() (we delegate this into the example because some examples need a different setup)
-        example.Configure(serviceCollection);
+                if (bool.TryParse(configuration["EnableTrace"], out var enableTrace) && enableTrace)
+                {
+                    builder.AddFilter("SharpBrick.PoweredUp.Bluetooth.BluetoothKernel", LogLevel.Debug);
+                }
+                if (bool.TryParse(configuration["TraceDebug"], out var traceDebug) && traceDebug)
+                {
+                    builder.AddFilter("SharpBrick.PoweredUp.BlueGigaBLE.BlueGigaBLEPoweredUpBluetoothAdapater", LogLevel.Debug);
+                }
+            });
+
+            // (2b) add .AddPoweredUp() (we delegate this into the example because some examples need a different setup)
+            example.Configure(serviceCollection);
 
 
-        // (2c) add your favourite Bluetooth Adapter
-        var bluetoothAdapter = configuration["BluetoothAdapter"] ?? "WinRT";
+            // (2c) add your favourite Bluetooth Adapter
+            var bluetoothAdapter = configuration["BluetoothAdapter"] ?? "WinRT";
 
 #if WINDOWS
         if (bluetoothAdapter == "WinRT")
@@ -81,27 +82,28 @@ class Program
 #endif
 
 #if NET5_0_OR_GREATER
-        if (bluetoothAdapter == "BlueGigaBLE")
-        {
-            // config for "COMPortName" and "TraceDebug" (either via command line or poweredup.json)
-            // on Windows-PCs you can find it under Device Manager --> Ports (COM & LPT) --> Bleugiga Bluetooth Low Energy (COM#) (where # is a number)
-            // "COMPortName": "COM4",
+            if (bluetoothAdapter == "BlueGigaBLE")
+            {
+                // config for "COMPortName" and "TraceDebug" (either via command line or poweredup.json)
+                // on Windows-PCs you can find it under Device Manager --> Ports (COM & LPT) --> Bleugiga Bluetooth Low Energy (COM#) (where # is a number)
+                // "COMPortName": "COM4",
 
-            // setting this option to false supresses the complete LogDebug()-commands; so they will not generated at all
-            // "TraceDebug": true
-            serviceCollection.AddBlueGigaBLEBluetooth(configuration);
-        }
+                // setting this option to false supresses the complete LogDebug()-commands; so they will not generated at all
+                // "TraceDebug": true
+                serviceCollection.AddBlueGigaBLEBluetooth(configuration);
+            }
 #endif
 
-        var serviceProvider = serviceCollection.BuildServiceProvider();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
-        // Examples Initialization
-        await example.InitExampleAndDiscoverAsync(serviceProvider, configuration);
+            // Examples Initialization
+            await example.InitExampleAndDiscoverAsync(serviceProvider, configuration);
 
-        // Example Execution
-        if (example.SelectedHub is not null)
-        {
-            await example.ExecuteAsync();
+            // Example Execution
+            if (example.SelectedHub is not null)
+            {
+                await example.ExecuteAsync();
+            }
         }
     }
 }
